4816f2a4521f4797cd4419b4d6bd8202d26c2054
student <pratiksuvarna@gmail.com>
2021-02-27 16:40:02 +0000

    Test Spring Pipeline

---
+++ src/main/java/com/example/CIPipeline/CiPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.CIPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CIPipeline/CiPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CIPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
